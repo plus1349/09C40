e317e559e842a3b31f6f1cf8178747630036ee09
Dmitriy <plus1349@gmail.com>
2020-10-28 00:26:05 +0200

    feat(abilities): add ru & uk locale

--- abilities/apps/abilities_config.py
+++ abilities/apps/abilities_config.py
@@ -4,4 +4,4 @@ from django.utils.translation import gettext_lazy as _
 
 class AbilitiesConfig(AppConfig):
     name = "abilities"
-    verbose_name = _("abilities")
+    verbose_name = _("Abilities")
